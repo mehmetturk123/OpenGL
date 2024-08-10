c86697d91265ec7616e78ab3f661303ddc280479
Mehmet Yağız Türk <yagizturk62@gmail.com>
2024-08-10 19:02:47 +0300

    Upgraded version of Hello World. It's ready for Hello Triangle

--- Source.cpp
+++ Source.cpp
@@ -2,15 +2,40 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+// The moment a user resizes the window the viewport should be adjusted as well. 
+// We can register a callback function on the window that gets called each time the window is resized.
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+// In case a GLFW function fails, an error is reported to the GLFW error callback.
+// You can receive these reports with an error callback.
+// This function must have the signature below but may do anything permitted in other callbacks.
+void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) 
+    {
+        std::cout << "Escape key pressed!\n";
+        glfwSetWindowShouldClose(window, true);
+    }
+}
 
 int main()
 {
+    // In case a GLFW function fails, an error is reported to the GLFW error callback
+    glfwSetErrorCallback(error_callback);
+
     // GLFW Initialization
     if(!glfwInit())
     {
         std::cout << "Initialization failed!\n";
-        return 1;
+        exit(EXIT_FAILURE);
     }
     std::cout << "Initialization successful!\n";
 
@@ -20,14 +45,14 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Window creation 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "ISelledMyWifeForInternetConnection", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "I Selled My Wife For Internet Connection", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window\n" << std::endl;
         glfwTerminate();
-        return -1;
+        exit(EXIT_FAILURE);
     }
-    std::cout << "Successfully created GLFW window\n";
+    std::cout << "Successfully created GLFW window!\n";
 
     // We tell GLFW to make the context of our window the main context on the current thread.
     glfwMakeContextCurrent(window);
@@ -36,7 +61,7 @@ int main()
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
+        exit(EXIT_FAILURE);
     }
     std::cout << "GLAD Initialization successful! \n";
     
@@ -49,17 +74,24 @@ int main()
     // Render loop
     while (!glfwWindowShouldClose(window))
     {
+        //input
+        processInput(window);
+
+        //rendering
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+        // check and call events and swap the buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    //Although every non-destroyed windows will be closed when glfwTerminate called, I will call destroy window for clarification.
+    glfwDestroyWindow(window);
+    std::cout << "Window destroyed!\n";
+
     glfwTerminate();
+    std::cout << "GLFW Terminated!\n";
 	return 0;
 }
 
-// The moment a user resizes the window the viewport should be adjusted as well. 
-// We can register a callback function on the window that gets called each time the window is resized.
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
