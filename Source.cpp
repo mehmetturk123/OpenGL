e2d4baec053aa0aa152aed3cbc5b14d5b3cd2d72
Mehmet Yağız Türk <yagizturk62@gmail.com>
2024-08-17 19:03:39 +0300

    Hello Textures.

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "Shader.h"
+#include "stb_image.h"
 #include <iostream>
 #include <filesystem>
 
@@ -101,45 +102,98 @@ int main()
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     
-    // Positions of my first Triangle
-    float vertices[] = {
-         // positions        // colors
-         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top
-    };
+    // Vertex data for Rectangle
+     float vertices[] = {
+          // positions         // colors           // texture coords
+          0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+          0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+         -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+         -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+     };
+     
+     unsigned int indices[]{ // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
+     };
 
-    // Create an unique ID corresponding to VBO (Vertex Buffer Object)
-    GLuint VBO;
+     unsigned int VAO, VBO, EBO;
+     glGenVertexArrays(1, &VAO);
+     glGenBuffers(1, &VBO);
+     glGenBuffers(1, &EBO);
 
-    // Generate one buffer object.
-    glGenBuffers(1, &VBO);
+     glBindVertexArray(VAO);
+     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+     glEnableVertexAttribArray(0);
+     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+     glEnableVertexAttribArray(1);
+     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+     glEnableVertexAttribArray(2);
 
-    // Bind the created buffer object above to the buffer type of VBO which is GL_ARRAY_BUFFER.
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-    // Lets fill this VBO.
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // Create an unique ID corresponding to VAO (Vertex Array Object) 
-    GLuint VAO;
-
-    // Generate one vertex array object.
-    glGenVertexArrays(1, &VAO);
-
-    // Bind Vertex Array Object
-    glBindVertexArray(VAO);
-
-    // Set our vertex attributes pointers
-    // ----------------------------------
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
 #pragma endregion
+#pragma region Texture Manipulation
 
+    unsigned int textures[2];
+    // First Texture 
+    //---------------
+    glGenTextures(2, textures);
+    glBindTexture(GL_TEXTURE_2D, textures[0]);
+
+    // set the texture wrapping/filtering options (on the currently bound texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // load and generate the texture
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load("C:\\Users\\mailt\\OneDrive\\Resimler\\Textures\\container.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    // Second Texture
+    //---------------
+    glBindTexture(GL_TEXTURE_2D, textures[1]);
+
+    // set the texture wrapping/filtering options (on the currently bound texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // load and generate the texture
+    stbi_set_flip_vertically_on_load(true);
+    data = stbi_load("C:\\Users\\mailt\\OneDrive\\Resimler\\Textures\\awesomeface.png", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+
+    ourShader.use(); // don't forget to activate the shader before setting uniforms!  
+    // We also have to tell OpenGL to which texture unit each shader sampler belongs to
+    ourShader.setInt("texture1", 0); 
+    ourShader.setInt("texture2", 1);
+
+
+#pragma endregion
 #pragma region Render Loop
     // Render loop
     // -----------
@@ -152,15 +206,14 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, textures[0]);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, textures[1]);
+
         ourShader.use();
-
-        float timeValue = glfwGetTime();
-        float offsetValue = (sin(timeValue) * 0.5f);
-        int vertexXOffsetLocation = glGetUniformLocation(ourShader.ID, "xOffset");
-        glUniform1f(vertexXOffsetLocation, offsetValue);
-
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // check and call events and swap the buffers
         glfwSwapBuffers(window);
@@ -173,6 +226,7 @@ int main()
     // ------------------------------------------------------------------------
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     glDeleteProgram(ourShader.ID);
 
--- vs.vert
+++ vs.vert
@@ -1,14 +1,14 @@
 #version 330 core
-layout (location = 0) in vec3 aPos;     // the position variable has attribute position 0
-layout (location = 1) in vec3 aColor;   // the color variable has attribute position 1
+layout (location = 0) in vec3 aPos;         // the position variable has attribute position 0
+layout (location = 1) in vec3 aColor;       // the color variable has attribute position 1
+layout (location = 2) in vec2 aTexCoord;    // the texture coordinate variable has attribute position 2
 
 out vec3 ourColor; // output color to the fragment shader
-
-uniform float xOffset;
+out vec2 texCoord; // output texture coordinate to the fragment shader
 
 void main()
 {
-    vec3 tempPos = vec3(aPos.x + xOffset, aPos. y, aPos.z);
-    gl_Position = vec4(tempPos, 1.0);
-    ourColor = aColor; // set ourColor to the input color we got from the vertex data
+	gl_Position = vec4(aPos, 1.0);
+	ourColor = aColor;
+	texCoord = vec2(aTexCoord.x, aTexCoord.y);
 }
